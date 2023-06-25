127c729e282a7a7b318710da86e12b014a914c71
Filip Chalupa <chalupa.filip@gmail.com>
2023-06-25 21:25:38 +0200

    Remove unintended build files

--- src/plugin.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.czechitasRenderVitePlugin = void 0;
-var plugin_react_1 = require("@vitejs/plugin-react");
-exports.czechitasRenderVitePlugin = plugin_react_1.default;
