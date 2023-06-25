d6e816f5dbc20f789f051f0e3eb6dc5939642239
Filip Chalupa <chalupa.filip@gmail.com>
2023-06-25 15:32:48 +0200

    Add types

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+import type { ReactElement } from 'react'
+
+export declare const render: (
+	rootElement: HTMLElement,
+	reactNode: ReactElement,
+) => void // @TODO: user types of the renderToString
+export declare const czechitasRenderVitePlugin: () => void // @TODO: use types of the original plugin
--- render.js
+++ render.js
@@ -1,9 +1,5 @@
 import { renderToString } from 'react-dom/server'
 
-/**
- * @param {HTMLElement} rootElement
- * @param {ReactElement} reactNode
- */
 export const render = (rootElement, reactNode) => {
 	const html = renderToString(reactNode)
 
